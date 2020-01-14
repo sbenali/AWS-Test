916c6f9e23ecbedfe6345a1c88f4c48491df4deb
sam2benali@gmail.com <sam2benali@gmail.com>
2020-01-14 22:49:13 +0000

    Initial commit.

---
+++ main.tf
@@ -0,0 +1,39 @@
+/*
+* main.tf
+*/
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "2.21.0"
+    # Assign IPv6 address on private subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  private_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on intra subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  intra_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on redshift subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  redshift_subnet_assign_ipv6_address_on_creation = false
+    # Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink = false
+    # Assign IPv6 address on public subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  public_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on database subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  database_subnet_assign_ipv6_address_on_creation = false
+    # Should be true to enable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink_dns_support = false
+    # Assign IPv6 address on elasticache subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  elasticache_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on private subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  private_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on intra subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  intra_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on redshift subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  redshift_subnet_assign_ipv6_address_on_creation = false
+    # Should be true to enable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink = false
+    # Assign IPv6 address on public subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  public_subnet_assign_ipv6_address_on_creation = false
+    # Assign IPv6 address on database subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  database_subnet_assign_ipv6_address_on_creation = false
+    # Should be true to enable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic.
+  enable_classiclink_dns_support = false
+    # Assign IPv6 address on elasticache subnet, must be disabled to change IPv6 CIDRs. This is the IPv6 equivalent of map_public_ip_on_launch
+  elasticache_subnet_assign_ipv6_address_on_creation = false
+}
